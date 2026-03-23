051a647b8cf822a73265f2ebf6d1334926c43d17
niyodushima <131856101+niyodushima@users.noreply.github.com>
2026-03-23 14:57:07 +0200

    Update auth.js

--- client/src/routes/auth.js
+++ client/src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-const User = require("./models/User"); // Mongoose model
+const User = require("../models/User"); // import User model
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
